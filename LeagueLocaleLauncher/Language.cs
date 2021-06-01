d9281cc2279a01abf6e52532552b0630522403e7
RyadaProductions <rodericks@live.nl>
2021-06-01 11:16:58 +0200

    Split files into 1 class/enum per file

---
+++ LeagueLocaleLauncher/Language.cs
@@ -0,0 +1,25 @@
+﻿namespace LeagueLocaleLauncher
+{
+    public enum Language
+    {
+        CHINESE_SIMPLIFIED,
+        CZECH_CZECH_REPUBLIC,
+        GERMAN_GERMANY,
+        GREEK_GREECE,
+        ENGLISH_AUSTRALIA,
+        ENGLISH_UNITED_KINGDOM,
+        ENGLISH_UNITED_STATES,
+        SPANISH_SPAIN,
+        SPANISH_MEXICO,
+        FRENCH_FRANCE,
+        HUNGARIAN_HUNGARY,
+        ITALIAN_ITALY,
+        JAPANESE_JAPAN,
+        KOREAN_KOREA,
+        POLISH_POLAND,
+        PORTUGUESE_BRAZIL,
+        ROMANIAN_ROMANIA,
+        RUSSIAN_RUSSIA,
+        TURKISH_TURKEY,
+    }
+}
---
+++ LeagueLocaleLauncher/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace LeagueLocaleLauncher
+{
+    // TODO: check what regions are actually supported, remove the rest
+    public enum Region
+    {
+        BR,
+        EUNE,
+        EUW,
+        JP,
+        KR, // doesn't work
+        LA1,
+        LA2,
+        NA,
+        OC1,
+        PBE, // doesn't work
+        RU,
+        TR,
+    }
+}
